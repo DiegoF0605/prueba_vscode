e94139242350997925e8df9d9e43654f41999920
DiegoF0605 <vidcara@gmail.com>
2026-06-23 21:32:34 -0600

    Más cambios

--- prueba.py
+++ prueba.py
@@ -1,1 +1,28 @@
 print('Hola terrícolas')
+
+x = 0
+y = 13
+if x < y:
+    print('x es menor que y')
+
+if x > y:
+    print('x es mayor que y')
+
+if x == y:
+    print('x es igual que y')
+    j = 90
+
+if x < y:
+    print('x es menor que y')
+
+else:
+    print('x no es menor que y')
+
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        print(f'Hola, mi nombre es {self.nombre} y tengo {self.edad} años.')
